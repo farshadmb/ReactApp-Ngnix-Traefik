c04345e29c54723402002080cab53b67641220b4
Farshad Mousalou <farshadm90@gmail.com>
2022-10-01 07:19:38 +0330

    - added dockers files.

--- web/src/app.module.ts
+++ web/src/app.module.ts
@@ -1,24 +1,24 @@
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import config from './config.json';
 import { join } from 'path';
 
+let serveConfig: any = {
+  rootPath: join(__dirname, '../../', 'app'),
+  renderPath: '*',
+  serveRoot: '/app',
+  exclude: ['api/*'],
+  serveStaticOptions: {
+    cacheControl: true,
+    maxAge: 5,
+    index: false,
+  },
+};
+
+serveConfig = config && Object.keys(config).length != 0 ? config : serveConfig;
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../../', 'build'),
-      renderPath: '*',
-      serveRoot: '/app',
-      exclude: ['api/*'],
-      serveStaticOptions: {
-        cacheControl: true,
-        maxAge: 5,
-        index: false,
-      },
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ServeStaticModule.forRoot(serveConfig)],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
